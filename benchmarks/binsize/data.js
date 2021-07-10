68dbe8f1a66585bba76064c95beafc0875c6e201
github-action-benchmark <github@users.noreply.github.com>
2021-07-10 00:04:18 +0000

    add Benchmark (cargo) benchmark result for a1b46119b4cd3b094f5d9ca711e4b44d45c4f3e0

---
+++ benchmarks/binsize/data.js
@@ -0,0 +1,122 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1625875458077,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "a1b46119b4cd3b094f5d9ca711e4b44d45c4f3e0",
+          "message": "Try gh-pages.",
+          "timestamp": "2021-07-09T16:58:25-07:00",
+          "tree_id": "ee3cd5efcc314aefbfdc6c5e8d8c8688f84aa459",
+          "url": "https://github.com/gnrunge/icu4x/commit/a1b46119b4cd3b094f5d9ca711e4b44d45c4f3e0"
+        },
+        "date": 1625875456499,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector",
+            "value": 17240,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "code_line_diff",
+            "value": 17978,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filter_langids",
+            "value": 21739,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tui",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "borrowed_pattern",
+            "value": 18901,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "simple",
+            "value": 921557,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "work_log",
+            "value": 18127,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "writeable_message",
+            "value": 13537,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "language_names_hash_map",
+            "value": 12498,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "permyriad",
+            "value": 12554,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "language_names_lite_map",
+            "value": 9312,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "unread_emails",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "syntatically_canonicalize_locales",
+            "value": 44330,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "elevator_floors",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "owned_pattern",
+            "value": 22573,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
