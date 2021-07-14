226237f37e1b214740dc71a3482365ea0a88cac6
github-action-benchmark <github@users.noreply.github.com>
2021-07-14 19:50:54 +0000

    add Benchmark (cargo) benchmark result for 41fabf5971895804a7a6dffbd1fc517e6a3958bd

--- benchmarks/binsize/data.js
+++ benchmarks/binsize/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626215312157,
+  "lastUpdate": 1626292253971,
   "repoUrl": "https://github.com/gnrunge/icu4x",
   "entries": {
     "Benchmark": [
@@ -251,6 +251,120 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gnrunge/icu4x/commit/e07219c50cb15b069c36d440ae8fe0e3f406768e"
         },
         "date": 1626215310369,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector",
+            "value": 17240,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "code_line_diff",
+            "value": 17978,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filter_langids",
+            "value": 21739,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tui",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "borrowed_pattern",
+            "value": 18901,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "simple",
+            "value": 921557,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "work_log",
+            "value": 18127,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "writeable_message",
+            "value": 13537,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "language_names_hash_map",
+            "value": 12498,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "permyriad",
+            "value": 12554,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "language_names_lite_map",
+            "value": 9312,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "unread_emails",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "syntatically_canonicalize_locales",
+            "value": 44330,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "elevator_floors",
+            "value": 18124,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "owned_pattern",
+            "value": 22573,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "41fabf5971895804a7a6dffbd1fc517e6a3958bd",
+          "message": "Remove test prints.",
+          "timestamp": "2021-07-14T12:42:27-07:00",
+          "tree_id": "9278c8967a5ecfa0a5768ccbcf551105479b776a",
+          "url": "https://github.com/gnrunge/icu4x/commit/41fabf5971895804a7a6dffbd1fc517e6a3958bd"
+        },
+        "date": 1626292252495,
         "tool": "cargo",
         "benches": [
           {
