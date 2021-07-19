a9909e3b2e54f117ff1b1724dc63f47c7537ede7
github-action-benchmark <github@users.noreply.github.com>
2021-07-19 22:50:21 +0000

    add Benchmark (ndjson) benchmark result for 9ec26cd8b3d75f33936244d6c1c34956996edf99

---
+++ benchmarks/binsize2/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1626735021202,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "9ec26cd8b3d75f33936244d6c1c34956996edf99",
+          "message": "Auto-push into unmerged-pr-bench-data on.",
+          "timestamp": "2021-07-19T15:43:34-07:00",
+          "tree_id": "6d25f5cb2a3f5e3f8eca586548dce36edc1dfab2",
+          "url": "https://github.com/gnrunge/icu4x/commit/9ec26cd8b3d75f33936244d6c1c34956996edf99"
+        },
+        "date": 1626735020687,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector",
+            "value": 17402,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "code_line_diff",
+            "value": 17978,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids",
+            "value": 21739,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "tui",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "borrowed_pattern",
+            "value": 18901,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "simple",
+            "value": 921549,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log",
+            "value": 18127,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "writeable_message",
+            "value": 13537,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_hash_map",
+            "value": 12498,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derive",
+            "value": 13067,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "permyriad",
+            "value": 12554,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_lite_map",
+            "value": 9312,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales",
+            "value": 44330,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "owned_pattern",
+            "value": 22573,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
