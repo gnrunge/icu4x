ca71b1369f20d7d577f82bee8647eeb0eb4eef60
github-action-benchmark <github@users.noreply.github.com>
2021-07-20 18:48:17 +0000

    add Benchmark (ndjson) benchmark result for 89a1f5ff0ad17b8ecadc7fc2a18507bfb57a02e2

--- benchmarks/binsize2/data.js
+++ benchmarks/binsize2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626735021202,
+  "lastUpdate": 1626806897186,
   "repoUrl": "https://github.com/gnrunge/icu4x",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,126 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gnrunge/icu4x/commit/9ec26cd8b3d75f33936244d6c1c34956996edf99"
         },
         "date": 1626735020687,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "unicode_bmp_blocks_selector",
+            "value": 17402,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "code_line_diff",
+            "value": 17978,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "filter_langids",
+            "value": 21739,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "tui",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "borrowed_pattern",
+            "value": 18901,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "simple",
+            "value": 921549,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "work_log",
+            "value": 18127,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "writeable_message",
+            "value": 13537,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_hash_map",
+            "value": 12498,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "derive",
+            "value": 13067,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "permyriad",
+            "value": 12554,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "language_names_lite_map",
+            "value": 9312,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "unread_emails",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "syntatically_canonicalize_locales",
+            "value": 44330,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "elevator_floors",
+            "value": 18124,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "owned_pattern",
+            "value": 22573,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "89a1f5ff0ad17b8ecadc7fc2a18507bfb57a02e2",
+          "message": "Investigating failing git checkout.",
+          "timestamp": "2021-07-20T11:40:56-07:00",
+          "tree_id": "6df49703f53d9286dfda4745150f6cfe3efae4d8",
+          "url": "https://github.com/gnrunge/icu4x/commit/89a1f5ff0ad17b8ecadc7fc2a18507bfb57a02e2"
+        },
+        "date": 1626806896333,
         "tool": "ndjson",
         "benches": [
           {
