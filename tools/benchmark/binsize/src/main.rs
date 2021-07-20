159576e68f0046d72da3fe1fb8d344a45d44cf42
gnrunge <nrunge@google.com>
2021-07-20 13:51:19 -0700

    Test run.

--- tools/benchmark/binsize/src/main.rs
+++ tools/benchmark/binsize/src/main.rs
@@ -11,11 +11,8 @@ fn wasm_filesize(dir: &str) -> Result<u64, std::io::Error> {
         if let Some(suffix) = p.extension() {
             if suffix == "wasm" {
                 println!(
-                    //"{{\"biggerIsBetter\":false,\"name\":{:?},\"unit\":\"bytes\",\"value\":{}}}",
-                    //p.file_stem().unwrap(),
-                    //p.metadata()?.len()
-                    "test {} ... bench:      {} ns/iter (+/- 0)",
-                    String::from(p.file_stem().unwrap().to_str().unwrap()),
+                    "{{\"biggerIsBetter\":false,\"name\":{:?},\"unit\":\"bytes\",\"value\":{}}}",
+                    p.file_stem().unwrap(),
                     p.metadata()?.len()
                 );
             }
