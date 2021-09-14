cc300ce172838fb3a0d2236c305646af6e09db17
github-action-benchmark <github@users.noreply.github.com>
2021-09-14 21:45:37 +0000

    add Benchmark (ndjson) benchmark result for f99acbae1f07d12611eb4fd9f56a82ed119602e6

---
+++ benchmarks/binsize/datasize/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1631655937949,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "f99acbae1f07d12611eb4fd9f56a82ed119602e6",
+          "message": "Set autopush to true.",
+          "timestamp": "2021-09-14T14:43:52-07:00",
+          "tree_id": "09cc9677219599478164759d6347d15a4eea9e0f",
+          "url": "https://github.com/gnrunge/icu4x/commit/f99acbae1f07d12611eb4fd9f56a82ed119602e6"
+        },
+        "date": 1631655937317,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 582678,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          },
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 582678,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
