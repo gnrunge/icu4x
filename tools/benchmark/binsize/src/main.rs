269523665efe6a99a0df3df0063aaea730426f05
gnrunge <nrunge@google.com>
2021-09-14 13:30:07 -0700

    Datasize benchmark beta.

--- tools/benchmark/binsize/src/main.rs
+++ tools/benchmark/binsize/src/main.rs
@@ -2,14 +2,19 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
-// This module takes as argument a directory path, looks for wasm
-// executables in the directory (i.e., extension .wasm), takes the size in
-// bytes of each, and writes the result in ndjson format to stdout.
+// This module has two modi operandi.
+// 1.: It takes a directory path, looks for wasm binaries in the directory
+// (i.e., extension .wasm), notes the size in bytes of each, and writes the
+// result in ndjson format to stdout. Likewise for benchmarking the gnuzip'd
+// wasm binaries.
+// 2.: It takes a path to a file and writes the file size in ndjson format
+// to stdout.
 // Note that the ICU4X wasm executables have to be build prior to
 // execution of this module. Use 'cargo make wasm-example'.
 use std::env;
 use std::fs;
 use std::process;
+
 
 fn wasm_filesize(dir: &str, filesuffix: &str) -> Result<u64, std::io::Error> {
     let paths = fs::read_dir(dir).expect("Directory with wasm binaries not found!");
@@ -31,23 +36,39 @@ fn wasm_filesize(dir: &str, filesuffix: &str) -> Result<u64, std::io::Error> {
     Ok(count)
 }
 
+fn any_file_size(filepath: &str) -> Result<bool, std::io::Error> {
+    let exists: bool = std::path::Path::new(filepath).exists();
+    if exists {
+        let fsize = fs::metadata(filepath)?.len();
+        println!( "{{\"biggerIsBetter\":false,\"name\":{:?},\"unit\":\"bytes\",\"value\":{}}}", filepath, fsize);
+    }
+    Ok(exists)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
         eprintln!("Usage: cargo run --package icu_benchmark_binsize -- <WASM BINARY DIRECTORY> <SUFFIX: wasm | gz>");
         process::exit(1);
     }
+    let path = &args[1];
+    let path_type = &args[2];
 
-    let wasmdir = &args[1];
-    let wasmsuffix = &args[2];
-    if wasmsuffix != "wasm" && wasmsuffix != "gz" {
-        eprintln!("Invalid file suffix {}, use wasm or gz", wasmsuffix);
+    if path_type != "wasm" && path_type != "gz" && path_type != "file" {
+        eprintln!("Invalid path type {}, use wasm or gz", path_type);
         process::exit(1);
     }
 
-    let count = wasm_filesize(wasmdir, wasmsuffix);
-    if count.unwrap() == 0 {
-        eprintln!("No wasm binaries found in directory {}", wasmdir);
-        process::exit(1);
+    if path_type == "file" {
+        let rc = any_file_size(path);
+        if !rc.unwrap() {
+            eprintln!("File {} not found", path);
+        }
+    } else {
+        let count = wasm_filesize(path, path_type);
+        if count.unwrap() == 0 {
+            eprintln!("No wasm binaries found in directory {}", path);
+            process::exit(1);
+        }
     }
 }
