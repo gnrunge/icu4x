6b7145d3b45b322ca183e330fc90a2780fbf5f98
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 21:20:11 +0000

    add Rust Benchmark (cargo) benchmark result for 8d518709289f4e0c7db8240264f9bcd6bc15ff7f

--- benchmarks/perf/components/locid/data.js
+++ benchmarks/perf/components/locid/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630011673754,
-  "repoUrl": "https://github.com/unicode-org/icu4x",
+  "lastUpdate": 1645824011480,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
   "entries": {
     "Rust Benchmark": [
       {
@@ -11801,6 +11801,42 @@ window.BENCHMARK_DATA = {
             "name": "locale/overview",
             "value": 4836,
             "range": "± 927",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "8d518709289f4e0c7db8240264f9bcd6bc15ff7f",
+          "message": "Remove commented-out parts, more testing.",
+          "timestamp": "2022-02-25T13:10:57-08:00",
+          "tree_id": "8527bd045d571f8cec5b0f456cca0811fc890be7",
+          "url": "https://github.com/gnrunge/icu4x/commit/8d518709289f4e0c7db8240264f9bcd6bc15ff7f"
+        },
+        "date": 1645824008968,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "langid/overview",
+            "value": 2207,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "locale/overview",
+            "value": 4102,
+            "range": "± 5",
             "unit": "ns/iter"
           }
         ]
