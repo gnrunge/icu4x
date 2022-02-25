4e2b20136d8849aeed3a55f3b9536a21e011bc85
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 19:44:53 +0000

    add Benchmark (ndjson) benchmark result for 337931e699bded2cc81860d89c740c4c42abaeed

---
+++ benchmarks/datasize/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1645818293469,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "337931e699bded2cc81860d89c740c4c42abaeed",
+          "message": "More changes and testing.",
+          "timestamp": "2022-02-25T11:42:59-08:00",
+          "tree_id": "5778f040a079e77780593453858a5c8ef3b5b807",
+          "url": "https://github.com/gnrunge/icu4x/commit/337931e699bded2cc81860d89c740c4c42abaeed"
+        },
+        "date": 1645818290937,
+        "tool": "ndjson",
+        "benches": [
+          {
+            "name": "provider/testdata/data/testdata.postcard",
+            "value": 1672368,
+            "unit": "bytes",
+            "biggerIsBetter": false
+          }
+        ]
+      }
+    ]
+  }
+}
