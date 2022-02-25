be788dd0a1d95b1981ac1b0a8afe014a57dc563b
github-action-benchmark <github@users.noreply.github.com>
2022-02-25 21:19:49 +0000

    add Rust Benchmark (cargo) benchmark result for 8d518709289f4e0c7db8240264f9bcd6bc15ff7f

---
+++ benchmarks/perf/utils/uniset/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1645823989753,
+  "repoUrl": "https://github.com/gnrunge/icu4x",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "committer": {
+            "email": "nrunge@google.com",
+            "name": "gnrunge",
+            "username": "gnrunge"
+          },
+          "distinct": true,
+          "id": "8d518709289f4e0c7db8240264f9bcd6bc15ff7f",
+          "message": "Remove commented-out parts, more testing.",
+          "timestamp": "2022-02-25T13:10:57-08:00",
+          "tree_id": "8527bd045d571f8cec5b0f456cca0811fc890be7",
+          "url": "https://github.com/gnrunge/icu4x/commit/8d518709289f4e0c7db8240264f9bcd6bc15ff7f"
+        },
+        "date": 1645823987095,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "uniset/overview",
+            "value": 45082804,
+            "range": "± 1696096",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
